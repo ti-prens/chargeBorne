5c97c38b85e885a5519888b916f52ae6fede0829
Prince Jacquet <jacquetped@gmail.com>
2020-11-28 13:56:41 +0100

    modification of voyants.c

--- lecteurcarte.c
+++ lecteurcarte.c
@@ -14,72 +14,82 @@ int num_client=0;
 
 
 void lecteurcarte_initialiser()
-{io=acces_memoire(&shmid);
-
-//ajouter un commentaire pour dire que la carte est ien initialisé
-/*associe la zone de memoire partagee au pointeur*/
-if(io==NULL) printf("Erreur pas de mem sh\n");
+{
+	io=acces_memoire(&shmid);
+	//ajouter un commentaire pour dire que la carte est ien initialisé
+	/*associe la zone de memoire partagee au pointeur*/
+	if(io==NULL) printf("Erreur pas de mem sh\n");
 }
 
 void lecteurcarte_lire_carte()
-{unsigned short int numero;
+{
+	unsigned short int numero;
+	
 	attente_insertion_carte();
 	timer_initialiser();
 	numero=lecture_numero_carte();
+	
 	printf("Badge inséré,numero lu %d \n", lecture_numero_carte());
 	
 	
 	if (baseclient_authentifier(numero)==1)
-		{num_client=numero;
-		printf("authentification OK \n ");
-		 blink_charge();
-		 while((valeur_timer())< 60 && (charge()==0 && stop()==0))
-		 {
-		 }
+		{
+			num_client=numero;
+			printf("authentification OK \n ");
+		 	voyants_blink_charge();
+		 	while((valeur_timer())< 60 && (charge()==0 && stop()==0))
+			{
+		 	}
 		 
-		 if(stop()==1)
-		 {io->bouton_stop=0;
-		 }
+		 	if(stop()==1)
+		 	{
+		 		io->bouton_stop=0;
+		 	}
 		 
-		 if(valeur_timer()>60)
-		 {printf("Temps ecoule!!! \n");
-		 lecteurcarte_initialiser();
-		 }
+			 if(valeur_timer()>60)
+		 	{
+		 		printf("Temps ecoule!!! \n");
+		 		lecteurcarte_initialiser();
+		 	}
 		 
-		 if(charge() == 1)
-		 	{io->bouton_charge=0;
-		 	printf("charge appui");
-		 	io->led_dispo=OFF;
-		 	charge();
-		 	//fin charge
-		 	attente_insertion_carte();
-		 	numero=lecture_numero_carte();
-		 	printf("numero lu %d \n",numero);
-		 	while(numero != num_client)
-		 		{usleep(1000000);
-		 		io->led_defaut=ROUGE;
-		 		usleep(1000000);
-		 		io->led_defaut=OFF;
+		 	if(charge() == 1)
+		 	{
+		 		io->bouton_charge=0;
+		 		printf("charge appui");
+		 		io->led_dispo=OFF;
+		 		charge();
+		 		//fin charge
 		 		attente_insertion_carte();
 		 		numero=lecture_numero_carte();
 		 		printf("numero lu %d \n",numero);
+		 		
+		 		while(numero != num_client)
+		 		{
+		 			usleep(1000000);
+		 			io->led_defaut=ROUGE;
+		 			usleep(1000000);
+		 			io->led_defaut=OFF;
+		 			attente_insertion_carte();
+		 			numero=lecture_numero_carte();
+		 			printf("numero lu %d \n",numero);
 		 		}
-		 	attente_retrait_carte();
-		 	//deconnecter();
-		 	/*while(tension()!=12)
-		 	{
-		 	}*/
+		 		attente_retrait_carte();
+		 		//deconnecter();
+		 		/*while(tension()!=12)
+		 		{
+		 		}*/
 		 	
-		 	//verouiller_trappe();
-		 	//io->led_prise=OFF;
-		 	//io->led_dispo=VERT;
+		 		//verouiller_trappe();
+		 		//io->led_prise=OFF;
+		 		//io->led_dispo=VERT;
 		 	}
 		 }
-		 else{
-		      printf("authentification echouée \n");
-		     lecteurcarte_initialiser();
-		     
-		     }
+	else
+	{
+		printf("authentification echouée \n");
+		lecteurcarte_initialiser();
+	}
+	
  }
 		
 	
--- voyants.c
+++ voyants.c
@@ -10,32 +10,46 @@ int shmid;
 //initialistation des voyants 
 void voyants_initialiser()
 {
-io=acces_memoire(&shmid);
-/*associe la zone de memoire partagee au pointeur*/
-if(io==NULL) printf("Erreur pas de mem sh\n");
+	io=acces_memoire(&shmid);
+	/*associe la zone de memoire partagee au pointeur*/
+	if(io==NULL) printf("Erreur pas de mem sh\n");
 }
 
 
-void set_charge()
+void voyants_set_charge(int x)
 {
-}
-void set_dispo()
-{io->led_dispo=VERT;
-}
-
-int dispo()
-{
-}
-
-
-void blink_charge()
-{
-int i;
-for(i=0;i<8;i++)
-	{usleep(500000);//suspend le processus en cours
-	 io->led_charge=VERT;
-	 usleep(500000);
-	 io->led_charge=OFF;
+	switch(x)
+	{
+		case 0 : io->led_charge=OFF; break;
+		case 1 : io->led_charge=ROUGE;break;
+		case 2 : io->led_charge=VERT;break;
+		default : io->led_charge=OFF;
+		printf("Erreur voyants_set_charge");
 	}
+}
 
+
+void voyants_set_dispo(int x)
+{
+	switch(x)
+	{
+		case 0 : io->led_dispo=OFF; break;
+		case 1 : io->led_dispo=ROUGE;break;
+		case 2 : io->led_dispo=VERT;break;
+		default : io->led_dispo=OFF;
+		printf("Erreur voyants_set_charge");
+	}
+}
+
+
+void voyants_blink_charge()
+{
+	int i;
+	for(i=0;i<8;i++)
+	{
+		usleep(500000);//suspend le processus en cours
+	 	voyants_set_charge(2);
+	 	usleep(500000);
+	 	voyants_set_charge(0);
+	}
 }
